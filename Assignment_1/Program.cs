c2b3d1e6d4aa5602529a447db8acc3c5c3600423
Alex <alexandersychev2005@gmai.com>
2023-02-10 11:55:24 +0200

    Starting first point

---
+++ Assignment_1/Program.cs
@@ -0,0 +1,49 @@
+﻿string variable = "21 + 32 - 567";
+List<string> operations = new List<string>();
+char[] operators = new[] { '+', '-', '*', '/' };
+
+string buff = "";
+char? oper = null;
+foreach (var s in variable)
+{
+    if (s == ' ')
+    {
+        continue;
+    }
+    
+    if (Char.IsDigit(s))
+    {
+        buff += s;
+    }
+    else if (operators.Contains(s))
+    {
+        if (oper is null)
+        {
+            operations.Add(buff);
+            buff = "";
+            oper = s;
+        }
+        else
+        {
+            operations.Add(buff);
+            operations.Add(oper.ToString());
+            buff = "";
+            oper = null;
+        }
+    }
+}
+
+if (buff != "")
+{
+    operations.Add(buff);
+}
+
+if (oper is not null)
+{
+    operations.Add(oper.ToString());
+}
+
+foreach (var op in operations)
+{
+    Console.WriteLine(op);
+}
