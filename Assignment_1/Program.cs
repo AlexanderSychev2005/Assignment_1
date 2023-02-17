45b40424cdc670c771580c5f6dc801a5c10919c8
Maryna <marynachernetska@gmail.com>
2023-02-17 12:48:04 +0200

    Plus class Stack

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -1,4 +1,6 @@
-﻿string input = "22 + 3*(56-     45)^435345";
+﻿using System.Xml;
+
+string input = Console.ReadLine();
 
 string b = "";
 string[] result = new String[50];
@@ -31,4 +33,48 @@ if (b.Length > 0)
 {
     result[index] = b;
 }
-Console.WriteLine("result: b=" + b);
+
+
+namespace Kse.Algorithms.Samples
+{
+    using System;
+
+    /// <summary>
+    /// This is a simple implementation of a limited capacity stack.
+    /// You can improve it by adding resizing or mote additional methods
+    /// </summary>
+    public class Stack
+    {
+        private const int Capacity = 50;
+
+        private string[] _array = new string[Capacity];
+
+        private int _pointer;
+
+        public void Push(string value)
+        {
+            if (_pointer == _array.Length)
+            {
+                // this code is raising an exception about reaching stack limit
+                throw new Exception("Stack overflowed");
+            }
+
+            _array[_pointer] = value;
+            _pointer++;
+        }
+
+        public string Pull()
+        {
+            if (_pointer == 0)
+            {
+                //you can also raise an exception here, but we're simple returning nothing
+                return null;
+            }
+
+            var value = _array[_pointer];
+            _pointer--;
+            return value;
+        }
+    }
+}
+ 
