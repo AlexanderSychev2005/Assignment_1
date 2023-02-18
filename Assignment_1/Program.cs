77bb6a02151e4ec0280863b9eaa497f20ebae070
Alex <alexandersychev2005@gmai.com>
2023-02-19 01:01:02 +0200

    KSE algorithm class Stack

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -1,61 +1,18 @@
-﻿using System.Xml;
-
-string input = Console.ReadLine();
-
-string b = "";
-string[] result = new String[50];
-int index = 0;
-
-foreach (var x in input)
-{
-    if (Char.IsDigit(x))
-    {
-        b += x;
-    }
-    else
-    { 
-        if (! (x == ' '))
-        {
-            if (b.Length > 0)
-            {
-                result[index] = b;
-                b = "";
-                index += 1;
-            }
-            result[index] = x.ToString();
-            index += 1;
-        }
-        
-    }
-}
-
-if (b.Length > 0)
-{
-    result[index] = b;
-}
-
+﻿using System;
+using System.Collections.Generic;
 
 namespace Kse.Algorithms.Samples
 {
-    using System;
-
-    /// <summary>
-    /// This is a simple implementation of a limited capacity stack.
-    /// You can improve it by adding resizing or mote additional methods
-    /// </summary>
     public class Stack
     {
         private const int Capacity = 50;
-
         private string[] _array = new string[Capacity];
-
         private int _pointer;
 
         public void Push(string value)
         {
             if (_pointer == _array.Length)
             {
-                // this code is raising an exception about reaching stack limit
                 throw new Exception("Stack overflowed");
             }
 
@@ -67,13 +24,13 @@ namespace Kse.Algorithms.Samples
         {
             if (_pointer == 0)
             {
-                //you can also raise an exception here, but we're simple returning nothing
                 return null;
             }
 
-            var value = _array[_pointer];
+            var value = _array[_pointer - 1];
             _pointer--;
             return value;
         }
     }
-}
+    
+    
